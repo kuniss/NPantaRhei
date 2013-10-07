bc3a67a1feb991162c8163fc53c91d1b004fabaa
Denis Kuniß <kuniss@grammarcraft.de>
2013-10-07 21:15:09 +0200

    tests for cyclic flow added -- native asynchronous NPantaRhrei oprations work, EBC operations not

---
+++ source/npantarhei.runtime/npantarhei.runtime.tests/cyclicflows/test_cyclic_flows.cs
@@ -0,0 +1,164 @@
+using System;
+using NUnit.Framework;
+using npantarhei.runtime.contract;
+using npantarhei.runtime.messagetypes;
+using npantarhei.runtime.operations;
+
+namespace npantarhei.runtime.tests.cyclicflows
+{
+	[TestFixture()]
+	public class test_cyclic_flows
+	{
+		internal const int maxCycles = 5000;
+
+		[Test()]
+		public void test_cyclic_flow_with_EBC_fails ()
+		{
+			Console.WriteLine("test started for {0} flow cycles...", maxCycles);
+
+            var config = new FlowRuntimeConfiguration()
+                                .AddStreamsFrom(@"
+                                                    /
+                                                    .in, cyclic.run
+                                                    cyclic.continue, cyclic.run
+                                                    cyclic.stopOn, .out
+                                                    ")
+                                .AddEventBasedComponent("cyclic", new CyclicFlow());
+
+            using(var fr = new FlowRuntime(config))
+            {
+                fr.UnhandledException += Console.WriteLine;
+
+                fr.Process(".in", 1);
+
+                Assert.IsFalse(fr.WaitForResult(5000), "StackOverflow exception expected but not thrown");
+            }
+		}
+		
+		[Test()]
+		public void test_cyclic_flow_with_async_EBC ()
+		{
+			Console.WriteLine("test started for {0} flow cycles...", maxCycles);
+
+            var config = new FlowRuntimeConfiguration()
+                                .AddStreamsFrom(@"
+                                                    /
+                                                    .in, cyclic.run
+                                                    cyclic.continue, cyclic.run
+                                                    cyclic.stopOn, .out
+                                                    ")
+                                .AddEventBasedComponent("cyclic", new CyclicFlow()).MakeAsync();
+
+            using(var fr = new FlowRuntime(config))
+            {
+                fr.UnhandledException += Console.WriteLine;
+
+                fr.Process(".in", 1);
+
+				// async EBC does not work comparing to native actions (see test_cyclic_flow_with_async_action), under clarfication...
+                Assert.IsFalse(fr.WaitForResult(5000));
+				
+				// this is whar we expected to work:
+//                IMessage result = null;
+//                Assert.IsTrue(fr.WaitForResult(10000, _ => result = _));
+//                Assert.AreEqual(".out", result.Port.Fullname);
+//                Assert.AreEqual(maxCycles, (int)result.Data);
+            }
+		}
+		
+		class CyclicFlow
+	    {			
+	        public void Run(int counter)
+	        {
+	            if (counter < maxCycles) {
+					Console.WriteLine("cycle {0}", counter);
+	                Continue(counter+1);
+				}
+	            else
+	                StopOn(maxCycles);
+	        }
+	
+	        public event Action<int> StopOn;
+	        public event Action<int> Continue;
+	    }
+
+	
+		[Test()]
+		public void test_cyclic_flow_with_sync_action_fails ()
+		{
+			Console.WriteLine("test started for {0} flow cycles...", maxCycles);
+
+            var config = new FlowRuntimeConfiguration()
+                                .AddStreamsFrom(@"
+                                                    /
+                                                    .in, cyclic.in
+                                                    cyclic.out0, cyclic.in
+                                                    cyclic.out1, .stopped
+                                                    ")
+                                .AddAction<int, int, int>("cyclic", 
+					                            (counter, continueCycling, stopOn) =>
+					                            {
+													if (counter < maxCycles) 
+													{
+														Console.WriteLine("cycle {0}", counter);
+														continueCycling(counter+1);
+													}
+													else{
+														stopOn(maxCycles);
+													}
+												}
+								);
+
+            using(var fr = new FlowRuntime(config))
+            {
+                fr.UnhandledException += Console.WriteLine;
+
+                fr.Process(".in", 1);
+
+                Assert.IsFalse(fr.WaitForResult(5000), "StackOverflow exception expected but not thrown");
+            }
+		}
+
+		[Test()]
+		public void test_cyclic_flow_with_async_action ()
+		{
+			Console.WriteLine("test started for {0} flow cycles...", maxCycles);
+
+            var config = new FlowRuntimeConfiguration()
+                                .AddStreamsFrom(@"
+                                                    /
+                                                    .in, cyclic.in
+                                                    cyclic.out0, cyclic.in
+                                                    cyclic.out1, .stopped
+                                                    ")
+                                .AddAction<int, int, int>("cyclic", 
+					                            (counter, continueCycling, stopOn) =>
+					                            {
+													if (counter < maxCycles) 
+													{
+														Console.WriteLine("cycle {0}", counter);
+														continueCycling(counter+1);
+													}
+													else{
+														stopOn(maxCycles);
+													}
+												}
+								).MakeAsync();
+
+            using(var fr = new FlowRuntime(config))
+            {
+                fr.UnhandledException += Console.WriteLine;
+
+                fr.Process(".in", 1);
+
+                IMessage result = null;
+                Assert.IsTrue(fr.WaitForResult(5000, _ => result = _));
+                Assert.AreEqual(".stopped", result.Port.Fullname);
+                Assert.AreEqual(maxCycles, (int)result.Data);
+            }
+		}
+
+	}
+	
+}
+

a3b3d5462b00aaf23dc40a482e8cc60a2c986336
Denis Kuniß <kuniss@grammarcraft.de>
2013-11-26 00:01:21 +0100

    made test_cyclic_flow_with_async_EBC running well by adding AsyncMethod attribute to the EBC input port method instead of calling MakeAsync for EBC operation wrapping; some code format improvements

--- source/npantarhei.runtime/npantarhei.runtime.tests/cyclicflows/test_cyclic_flows.cs
+++ source/npantarhei.runtime/npantarhei.runtime.tests/cyclicflows/test_cyclic_flows.cs
@@ -6,13 +6,13 @@ using npantarhei.runtime.operations;
 
 namespace npantarhei.runtime.tests.cyclicflows
 {
-	[TestFixture()]
+	[TestFixture]
 	public class test_cyclic_flows
 	{
 		internal const int maxCycles = 5000;
-
-		[Test()]
-		public void test_cyclic_flow_with_EBC_fails ()
+		
+		[Test]
+		public void test_cyclic_flow_with_async_EBC ()
 		{
 			Console.WriteLine("test started for {0} flow cycles...", maxCycles);
 
@@ -31,43 +31,17 @@ namespace npantarhei.runtime.tests.cyclicflows
 
                 fr.Process(".in", 1);
 
-                Assert.IsFalse(fr.WaitForResult(5000), "StackOverflow exception expected but not thrown");
-            }
-		}
-		
-		[Test()]
-		public void test_cyclic_flow_with_async_EBC ()
-		{
-			Console.WriteLine("test started for {0} flow cycles...", maxCycles);
-
-            var config = new FlowRuntimeConfiguration()
-                                .AddStreamsFrom(@"
-                                                    /
-                                                    .in, cyclic.run
-                                                    cyclic.continue, cyclic.run
-                                                    cyclic.stopOn, .out
-                                                    ")
-                                .AddEventBasedComponent("cyclic", new CyclicFlow()).MakeAsync();
-
-            using(var fr = new FlowRuntime(config))
-            {
-                fr.UnhandledException += Console.WriteLine;
-
-                fr.Process(".in", 1);
-
-				// async EBC does not work comparing to native actions (see test_cyclic_flow_with_async_action), under clarfication...
-                Assert.IsFalse(fr.WaitForResult(5000));
-				
-				// this is whar we expected to work:
-//                IMessage result = null;
-//                Assert.IsTrue(fr.WaitForResult(10000, _ => result = _));
-//                Assert.AreEqual(".out", result.Port.Fullname);
-//                Assert.AreEqual(maxCycles, (int)result.Data);
+                IMessage result = null;
+                Assert.IsTrue(fr.WaitForResult(5000, _ => result = _), 
+				              "result not received within 5 seconds");
+                Assert.AreEqual(".out", result.Port.Fullname);
+                Assert.AreEqual(maxCycles, (int)result.Data);
             }
 		}
 		
 		class CyclicFlow
 	    {			
+			[AsyncMethod]
 	        public void Run(int counter)
 	        {
 	            if (counter < maxCycles) {
@@ -83,7 +57,7 @@ namespace npantarhei.runtime.tests.cyclicflows
 	    }
 
 	
-		[Test()]
+		[Test]
 		public void test_cyclic_flow_with_sync_action_fails ()
 		{
 			Console.WriteLine("test started for {0} flow cycles...", maxCycles);
@@ -119,7 +93,7 @@ namespace npantarhei.runtime.tests.cyclicflows
             }
 		}
 
-		[Test()]
+		[Test]
 		public void test_cyclic_flow_with_async_action ()
 		{
 			Console.WriteLine("test started for {0} flow cycles...", maxCycles);
@@ -152,7 +126,8 @@ namespace npantarhei.runtime.tests.cyclicflows
                 fr.Process(".in", 1);
 
                 IMessage result = null;
-                Assert.IsTrue(fr.WaitForResult(5000, _ => result = _));
+                Assert.IsTrue(fr.WaitForResult(5000, _ => result = _), 
+				              "result not received with 5 seconds");
                 Assert.AreEqual(".stopped", result.Port.Fullname);
                 Assert.AreEqual(maxCycles, (int)result.Data);
             }
